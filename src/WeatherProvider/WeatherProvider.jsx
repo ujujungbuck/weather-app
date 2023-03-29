31e1180148b338dc8f184f4adb67d0e53f43d98a
ujujungbuck <garam8u2@gmail.com>
2023-03-29 18:32:50 +0900

    날씨앱만들기

---
+++ src/WeatherProvider/WeatherProvider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const WeatherContext = createContext({})
+
+function WeatherProvider() {
+    return (
+        <div>WeatherProvider</div>
+    )
+}
+
+export default WeatherProvider
